6d6c939082ac5b2fa3232c5510e0abba988d9b68
Lasana Murray <lmurray@quenk.com>
2022-12-18 19:52:55 -0400

    chore: move board-validators into board-server

--- packages/board-server/src/data/validators/common.ts
+++ packages/board-server/src/data/validators/common.ts
@@ -4,12 +4,13 @@ import * as array from '@quenk/preconditions/lib/array';
 import { Value } from '@quenk/noni/lib/data/jsonx';
 import { Precondition, and } from '@quenk/preconditions';
 import { contains } from '@quenk/noni/lib/data/array';
+import { parseDate } from '@quenk/noni/lib/data/datetime';
 
 import { succeed, fail } from '@quenk/preconditions/lib/result';
 
-import { supportedCurrencies } from '@devcarib/common/lib/data/currency';
-import { supportedPaymentFrequencies } from '@devcarib/common/lib/data/payment';
-import { jobStatuses } from '@devcarib/common/lib/data/job';
+import { supportedCurrencies } from '../currency';
+import { supportedPaymentFrequencies } from '../payment';
+import { jobStatuses } from '../job';
 
 /**
  * name must be a string and between 1-64 characters.
@@ -23,8 +24,19 @@ export const name: Precondition<Value, string> =
  * email must be a string between 3-64 characters and contain "@".
  */
 export const email: Precondition<Value, string> =
-    and(string.isString, and(
-        and(string.minLength(3), string.maxLength(64)), string.matches(/@/)));
+    and(string.isString,
+        and(string.lowercase,
+            and(and(string.minLength(3), string.maxLength(64)),
+                string.matches(/@/))));
+
+/**
+ * username must be 3-12 characters and must begin with a letter.
+ */
+export const username: Precondition<Value, string> =
+    and(string.isString,
+        and(string.lowercase,
+            and(and(string.minLength(3), string.maxLength(12)),
+                string.matches(/^[a-z][0-9a-z$@_]+/))));
 
 /**
  * password must be a string between 8-140 characters.
@@ -36,8 +48,10 @@ export const password: Precondition<Value, string> =
  * url must be a string of at least 7 characters and begin with http or https.
  */
 export const url: Precondition<Value, string> =
-    and(string.isString, and(and(string.minLength(7), string.maxLength(5000)),
-        string.matches(/^(http|https):\/\//)));
+    and(string.isString,
+        and(string.lowercase,
+            and(and(string.minLength(7), string.maxLength(5000)),
+                string.matches(/^(http|https):\/\//))));
 
 /**
  * textsmall is 256 characters or less.
@@ -74,6 +88,40 @@ export const maxLength = (n: number): Precondition<Value, Value> =>
         string.maxLength(n)(<string>value);
 
 /**
+ * date must be a valid ISO8601 date.
+ */
+export const date: Precondition<Value, Value> = (value: Value) => {
+
+    let dateString = parseDate(String(value));
+
+    return (dateString !== '') ? succeed(dateString) : fail('date', { value });
+
+}
+
+const timeRegex = /^([01][0-9]|2[0-3]):([0-5][0-9])$/;
+
+/**
+ * time only supports hh:mm.
+ */
+export const time: Precondition<Value, Value> = (value: Value) =>
+    timeRegex.test(String(value)) ? succeed(value) : fail('time', { value });
+
+const tzoffsetRegex = /^[+-][01][0-9]:[0-5][0-9]$/;
+
+/**
+ * tzoffset in the format +04:00
+ */
+export const tzoffset: Precondition<Value, Value> = (value: Value) =>
+    tzoffsetRegex.test(String(value)) ?
+        succeed(value) : fail('tzoffset', { value });
+
+/**
+ * boolean casts a value to a JS boolean.
+ */
+export const boolean: Precondition<Value, Value> = (value: Value) =>
+    succeed(Boolean(value));
+
+/**
  * currency ensures the provided string is one of the supported currency
  * indicators.
  */
@@ -97,4 +145,4 @@ export const paymentFrequency: Precondition<Value, Value> = (value: Value) =>
 export const jobStatus: Precondition<Value, Value> = (value: Value) =>
     contains(jobStatuses, value) ?
         succeed(value) :
-        fail('invalid', { value })
+        fail('invalid', { value });
